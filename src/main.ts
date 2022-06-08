6e8bdb7aeb3634043bcbdb28ecef564b6c084648
Eric Afes <eriicafes@gmail.com>
2022-06-09 00:18:13 +0100

    Add global validation pipes and entity interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './core/services/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // validate route input
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true
+    }
+  }))
+
+  // transform response entity
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app)
+
   await app.listen(3000);
 }
 bootstrap();
