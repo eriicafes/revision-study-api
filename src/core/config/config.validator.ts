9ef424917ff4a5138f0e4156745694a19fbaad39
Eric Afes <eriicafes@gmail.com>
2022-06-08 20:27:15 +0100

    Add core module and export custom AppConfigService
    Added a new global module named core which import ConfigModule from @nestjs/config and exports our AppConfigService
    
    Validation is applied to environment variables using class-validator and class-transformer
    
    AppConfigService wraps around @nestjs/config ConfigService's get method to make it type safe

---
+++ src/core/config/config.validator.ts
@@ -0,0 +1,20 @@
+import { plainToClass } from "class-transformer";
+import { validateSync } from "class-validator";
+
+export class EnvironmentVariables { }
+
+export function validateConfig(config: Record<string, unknown>): EnvironmentVariables {
+    const configObj = plainToClass(EnvironmentVariables, config, { enableImplicitConversion: true })
+
+    const errors = validateSync(configObj)
+
+    if (errors.length > 0) {
+        const errorMessages = errors.reduce((acc, error) => {
+            return [...acc, ...Object.values(error.constraints)]
+        }, [])
+        console.log("Configuration error:", errorMessages.map(e => "\n* " + e).join(""))
+        throw new Error("Configuration error")
+    }
+
+    return configObj
+}
